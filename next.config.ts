f10a19697b783563064bdc8938895ee5e23e0d5f
Vinicius Santos <vinicius.vph@gmail.com>
2026-04-13 10:26:57 +0100

    refactor: minor changes

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,11 @@ const nextConfig: NextConfig = {
         hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
